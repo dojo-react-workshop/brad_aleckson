7cfcf718eaf09d07759b9031ab7bf386933a8165
Brad Aleckson <bradaleckson@northwesternmutual.com>
2017-05-22 10:41:09 -0500

    added functionality to filter and map

--- coding_challenges/filter.js
+++ coding_challenges/filter.js
@@ -1,1 +1,14 @@
-Array.prototype.filter = function(){}
+'use strict';
+
+Array.prototype.filter = function(callback){
+    const newArray = [];
+    this.forEach(function(value){
+        if(callback(value))
+            newArray.push(value);
+    })
+
+    return newArray;
+}
+
+const a = [1,2,3,4,5,6,7,8,9,10];
+console.log(a.filter(function(val){return val%2 === 0}));
--- coding_challenges/map.js
+++ coding_challenges/map.js
@@ -1,1 +1,23 @@
-Array.prototype.map = function(){}
+'use strict';
+
+Array.prototype.map = function(callback){
+    const newArray = [];
+    this.forEach(function(value){
+        newArray.push(callback(value));
+    })
+    
+    // for(let i = 0; i < this.length - 1; i++)
+    // {
+    //     newArray.push(callback(value));
+    // }
+
+    // to be consistent with full JavaScript function 
+    // this.forEach(function(value, index, array){
+    //     newArray.push(callback(value, index, array));
+    // })
+
+    return newArray;
+}
+
+const a = [1,2,3];
+console.log(a.map(function(val){return val * val}));
